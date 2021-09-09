611ae7a8165e25e155a37f88699ca5fcd873b512
Edmetrio <edmetrio06@gmail.com>
2021-09-09 10:24:44 +0200

    relacionamento de telefone

--- app/Models/Models/Operadora.php
+++ app/Models/Models/Operadora.php
@@ -4,8 +4,20 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class \Operadora extends Model
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+class Operadora extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'operadora';
+    protected $fillable = ['nome'];
+
+    public function telefones()
+    {
+        return $this->hasMany(Telefone::class, 'operadora_id');
+    }
 }
