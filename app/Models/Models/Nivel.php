c07e2551f60b584066a179c96bebe46a1766243c
Edmetrio <edmetrio06@gmail.com>
2021-09-09 10:24:22 +0200

    dados do nível

--- app/Models/Models/Nivel.php
+++ app/Models/Models/Nivel.php
@@ -4,8 +4,16 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class nivel extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'nivel';
+    protected $fillable = ['nome','estado'];
 }
