0985de66af17a05160479c08de2038145b54a9e2
Edmetrio <edmetrio06@gmail.com>
2021-09-09 10:25:04 +0200

    relacionamento de endereços

--- app/Models/Models/Perfil.php
+++ app/Models/Models/Perfil.php
@@ -2,10 +2,34 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Perfil extends Model
+class Perfil extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'perfil';
+    protected $fillable = ['users_id','nuit','nome','endereco'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
+    public function telefones()
+    {
+        return $this->hasMany(Telefone::class, 'perfil_id');
+    }
+
+    public function enderecos()
+    {
+        return $this->hasMany(Endereco::class, 'perfil_id');
+    }
 }
