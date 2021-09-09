96848c7e615374891d931d9dc2d0e632e89e0a83
Edmetrio <edmetrio06@gmail.com>
2021-09-09 10:25:30 +0200

    relaconamento de perfil

--- app/Models/Models/Telefone.php
+++ app/Models/Models/Telefone.php
@@ -4,8 +4,26 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Telefone extends Model
+class Telefone extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'telefone';
+    protected $fillable = ['perfil_id','operadora_id','numero','estado'];
+
+    public function operadoras()
+    {
+        return $this->hasOne(Operadora::class, 'id', 'operadora_id');
+    }
+
+    public function perfils()
+    {
+        return $this->hasOne(Perfil::class, 'id', 'perfil_id');
+    }
 }
