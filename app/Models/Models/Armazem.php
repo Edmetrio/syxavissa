3b0125b94da1d624e29873280e32d55e6ae30338
Edmetrio <edmetrio06@gmail.com>
2021-09-09 10:23:56 +0200

    relacionamento de perfil

--- app/Models/Models/Armazem.php
+++ app/Models/Models/Armazem.php
@@ -5,7 +5,7 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class \Armazem extends Model
+class Armazem extends Model
 {
     use HasFactory;
 }
--- app/Models/Models/Endereco.php
+++ app/Models/Models/Endereco.php
@@ -4,8 +4,20 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class \Endereco extends Model
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+class Endereco extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'endereco';
+    protected $fillable = ['perfil_id', 'nome','estado'];
+
+    public function perfils()
+    {
+        return $this->hasOne(Perfil::class, 'id', 'perfil_id');
+    }
 }
