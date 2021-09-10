ed425d5fd63586834862b746da5baeecf0377dc1
Edmetrio <edmetrio06@gmail.com>
2021-09-10 21:47:46 +0200

    modelo tipo de transação

--- app/Models/Models/Tipotransacao.php
+++ app/Models/Models/Tipotransacao.php
@@ -4,8 +4,16 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Tipotransacao extends Model
+class Tipotransacao extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'tipotransacao';
+    protected $fillable = ['nome','estado'];
 }
