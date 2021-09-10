ad3f20b9e410b87fd2837269c2aa45334abc0412
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:38:55 +0200

    dados de model tipo

--- app/Models/Models/Tipo.php
+++ app/Models/Models/Tipo.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Tipo extends Model
+class Tipo extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'tipo';
+    protected $fillable = ['nome','estado'];
+
+    public function Artigos()
+    {
+        return $this->hasMany(Artigo::class, 'tipo_id');
+    }
 }
