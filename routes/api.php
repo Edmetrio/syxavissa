938a0d3817abb2b3708cf41517f731f955b01e8e
Edmetrio <edmetrio06@gmail.com>
2021-09-10 21:48:50 +0200

    rotas

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,30 @@
 
 use App\Http\Controllers\api\ArmazemController;
 use App\Http\Controllers\api\ArtigoController;
+use App\Http\Controllers\api\AumentoController;
 use App\Http\Controllers\api\CategoriaController;
 use App\Http\Controllers\api\ComposicaoController;
 use App\Http\Controllers\api\EnderecoController;
+use App\Http\Controllers\api\HistoricoController;
+use App\Http\Controllers\api\ItemhistoricoController;
+use App\Http\Controllers\api\ItemtransacaoController;
 use App\Http\Controllers\api\NivelController;
 use App\Http\Controllers\api\OperadoraController;
+use App\Http\Controllers\api\PagamentoController;
 use App\Http\Controllers\Api\PerfilController;
 use App\Http\Controllers\api\StockController;
 use App\Http\Controllers\api\SubcategoriaController;
 use App\Http\Controllers\api\TelefoneController;
 use App\Http\Controllers\api\TipoController;
+use App\Http\Controllers\api\TipotransacaoController;
+use App\Http\Controllers\api\TransacaoController;
 use App\Http\Controllers\api\UnidadeController;
 use App\Models\Models\Artigo;
 use App\Models\Models\Endereco;
+use App\Models\Models\Itemhistorico;
+use App\Models\Models\Itemtransacao;
 use App\Models\Models\Telefone;
+use App\Models\Models\Tipotransacao;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -57,3 +67,17 @@ Route::apiResource('artigo', ArtigoController::class);
 Route::apiResource('composicao', ComposicaoController::class);
 
 Route::apiResource('stock', StockController::class);
+
+Route::apiResource('pagamento', PagamentoController::class);
+
+Route::apiResource('tipotransacao', TipotransacaoController::class);
+
+Route::apiResource('transacao', TransacaoController::class);
+
+Route::apiResource('itemtransacao', ItemtransacaoController::class);
+
+Route::apiResource('aumento', AumentoController::class);
+
+Route::apiResource('historico', HistoricoController::class);
+
+Route::apiResource('itemhistorico', ItemhistoricoController::class);
