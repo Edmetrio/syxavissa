38c8700980d55030d002ba61dcb54f5cf9e4fbce
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:39:48 +0200

    dados do model e relacionamento

--- app/Models/Models/Unidade.php
+++ app/Models/Models/Unidade.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Unidade extends Model
+class Unidade extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'unidade';
+    protected $fillable = ['nome','estado'];
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class, 'unidade_id');
+    }
 }
