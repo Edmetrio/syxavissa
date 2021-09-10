314dbfe5cb43f32166b4748e5c21921cb9dbceee
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:21:18 +0200

    dados do model e relacionamento stock

--- app/Models/Models/Armazem.php
+++ app/Models/Models/Armazem.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Armazem extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'armazem';
+    protected $fillable = ['numero','nome','local','estado'];
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class, 'armazem_id');
+    }
 }
