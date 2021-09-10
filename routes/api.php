31c5983260b2fc8f89f5a3a2cbfe6159a40c202d
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:40:21 +0200

    rotas

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\api\ArmazemController;
+use App\Http\Controllers\api\ArtigoController;
+use App\Http\Controllers\api\CategoriaController;
+use App\Http\Controllers\api\ComposicaoController;
 use App\Http\Controllers\api\EnderecoController;
 use App\Http\Controllers\api\NivelController;
 use App\Http\Controllers\api\OperadoraController;
 use App\Http\Controllers\Api\PerfilController;
+use App\Http\Controllers\api\StockController;
+use App\Http\Controllers\api\SubcategoriaController;
 use App\Http\Controllers\api\TelefoneController;
+use App\Http\Controllers\api\TipoController;
+use App\Http\Controllers\api\UnidadeController;
+use App\Models\Models\Artigo;
 use App\Models\Models\Endereco;
 use App\Models\Models\Telefone;
 use Illuminate\Http\Request;
@@ -30,3 +39,21 @@ Route::apiResource('telefone', TelefoneController::class);
 Route::apiResource('operadora', OperadoraController::class);
 
 Route::apiResource('endereco', EnderecoController::class);
+
+Route::apiResource('categoria', CategoriaController::class);
+
+Route::apiResource('subcategoria', SubcategoriaController::class);
+
+Route::apiResource('tipo', TipoController::class);
+
+Route::apiResource('unidade', UnidadeController::class);
+
+Route::apiResource('armazem', ArmazemController::class);
+
+Route::apiResource('stock', StockController::class);
+
+Route::apiResource('artigo', ArtigoController::class);
+
+Route::apiResource('composicao', ComposicaoController::class);
+
+Route::apiResource('stock', StockController::class);
