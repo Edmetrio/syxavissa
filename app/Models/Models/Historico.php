50e60985f4dd30dd39205e3335e6a580cb0a7c16
Edmetrio <edmetrio06@gmail.com>
2021-09-10 21:46:17 +0200

    model historico

--- app/Models/Models/Historico.php
+++ app/Models/Models/Historico.php
@@ -2,10 +2,34 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Historico extends Model
+class Historico extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'historico';
+    protected $fillable = ['users_id','userscli_id','pagamento_id','tipotransacao_id','validade','datapagamento','banco','caixa','subtotal','iva','desconto','valortotal','valorpago','valordevido','estado'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
+    public function pagamentos()
+    {
+        return $this->hasOne(Pagamento::class, 'id', 'pagamento_id');
+    }
+
+    public function tipotransacaos()
+    {
+        return $this->hasOne(Tipotransacao::class, 'id', 'tipotransacao_id');
+    }
 }
