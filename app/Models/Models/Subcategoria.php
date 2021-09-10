3a1255e53715ed0a83215947a6e8ac7217413456
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:38:40 +0200

    dados do model e relacionamento com artigo

--- app/Models/Models/Subcategoria.php
+++ app/Models/Models/Subcategoria.php
@@ -4,8 +4,26 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Subcategoria extends Model
+class Subcategoria extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    
+    protected $table = 'subcategoria';
+    protected $fillable = ['categoria_id','nome','icon','estado'];
+
+    public function categorias()
+    {
+        return $this->hasOne(Categoria::class, 'id', 'categoria_id');
+    }
+
+    public function artigos()
+    {
+        return $this->hasMany(Artigo::class, 'subcategoria_id');
+    }
 }
