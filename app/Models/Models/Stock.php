c1bae591098d0448942c45b8f04411f84a966f9d
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:33:22 +0200

    dados do model e relaciomanemto

--- app/Models/Models/Stock.php
+++ app/Models/Models/Stock.php
@@ -2,10 +2,40 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Stock extends Model
+
+class Stock extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'stock';
+    protected $fillable = ['users_id','artigo_id','unidade_id','armazem_id','custo','quantidade','stockminimo','estado'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
+    public function artigos()
+    {
+        return $this->hasOne(Artigo::class, 'id', 'artigo_id');
+    }
+
+    public function unidades()
+    {
+        return $this->hasOne(Unidade::class, 'id', 'unidade_id');
+    }
+
+    public function armazens()
+    {
+        return $this->hasOne(Armazem::class, 'id', 'armazem_id');
+    }
 }
