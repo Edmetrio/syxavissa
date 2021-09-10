2d86b128fafbe1498acb2bb78503dbe14fa91a78
Edmetrio <edmetrio06@gmail.com>
2021-09-10 21:47:13 +0200

    modelo item transação

--- app/Models/Models/Itemtransacao.php
+++ app/Models/Models/Itemtransacao.php
@@ -4,8 +4,26 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Itemtransacao extends Model
+class Itemtransacao extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'itemtransacao';
+    protected $fillable = ['transacao_id','artigo_id','quantidade','preco','iva','desconto','subtotal','estado'];
+
+    public function transacaos()
+    {
+        return $this->hasOne(transacao::class, 'id', 'transacao_id');
+    }
+
+    public function artigos()
+    {
+        return $this->hasOne(Artigo::class, 'id', 'artigo_id');
+    }    
 }
