5c4da370ab96ea36b5403d9738b8a9a8e877cb4c
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:23:18 +0200

    model e dados da categoria

--- app/Models/Models/Categoria.php
+++ app/Models/Models/Categoria.php
@@ -4,8 +4,26 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Categoria extends Model
+class Categoria extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'categoria';
+    protected $fillable = ['nome','icon','estado'];
+
+    public function subcategorias()
+    {
+        return $this->hasMany(Subcategoria::class, 'categoria_id');
+    }
+
+    public function artigos()
+    {
+        return $this->hasMany(Artigo::class, 'categoria_id');
+    }
 }
