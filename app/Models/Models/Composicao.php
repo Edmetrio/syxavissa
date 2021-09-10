06aebeddfdaa1026bf979605f889e5aae7dc32a0
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:33:03 +0200

    dados da composição e relacionamento unidades

--- app/Models/Models/Composicao.php
+++ app/Models/Models/Composicao.php
@@ -4,8 +4,26 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Composicao extends Model
+class Composicao extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'composicao';
+    protected $fillable = ['artigo_id','unidade_id','quantidade','estado'];
+
+    public function artigos()
+    {
+        return $this->hasOne(Artigo::class, 'id', 'artigo_id');
+    }
+
+    public function unidades()
+    {
+        return $this->hasOne(Unidade::class, 'id', 'unidade_id');
+    }
 }
