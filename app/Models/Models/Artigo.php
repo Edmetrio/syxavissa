74e78262173bf99db3bc787193da2d48c7f233f8
Edmetrio <edmetrio06@gmail.com>
2021-09-10 09:22:06 +0200

    model de tipos

--- app/Models/Models/Artigo.php
+++ app/Models/Models/Artigo.php
@@ -4,8 +4,36 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Artigo extends Model
+class Artigo extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'artigo';
+    protected $fillable = ['codigobarra','categoria_id','subcategoria_id','tipo_id','nome','icon','preco','iva','desconto','garantia','estado'];
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class, 'artigo_id');
+    }
+
+    public function categorias()
+    {
+        return $this->hasOne(Categoria::class, 'id', 'categoria_id');
+    }
+
+    public function subcategorias()
+    {
+        return $this->hasOne(Subcategoria::class, 'id', 'subcategoria_id');
+    }
+
+    public function tipos()
+    {
+        return $this->hasOne(Tipo::class, 'id', 'tipo_id');
+    }
 }
