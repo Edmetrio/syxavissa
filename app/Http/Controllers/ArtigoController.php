afdfe7190185d70f7ea4d0c3c43f7c56a4a37d5a
Edmetrio <edmetrio06@gmail.com>
2021-09-24 15:59:40 +0200

    update crud

--- app/Http/Controllers/ArtigoController.php
+++ app/Http/Controllers/ArtigoController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Models\Armazem;
 use App\Models\Models\Artigo;
 use App\Models\Models\Categoria;
+use App\Models\Models\Stock;
 use App\Models\Models\Subcategoria;
 use App\Models\Models\Tipo;
+use App\Models\Models\Unidade;
 use Illuminate\Http\Request;
 
 class ArtigoController extends Controller
@@ -17,7 +20,7 @@ class ArtigoController extends Controller
      */
     public function index()
     {
-        $artigo = Artigo::with(['categorias','subcategorias','tipos'])->orderBy('id', 'desc')->get();
+        $artigo = Artigo::with(['categorias','subcategorias','tipos','stocks'])->orderBy('id', 'desc')->get();
         return view('artigo', compact('artigo')); 
     }
 
@@ -28,22 +31,56 @@ class ArtigoController extends Controller
      */
     public function create()
     {
-        $artigo = Artigo::with(['categorias','subcategorias','tipos'])->orderBy('id', 'desc')->get();
         $categoria = Categoria::orderBy('id', 'desc')->get();
         $tipo = Tipo::orderBy('id', 'desc')->get();
         $subcategoria = Subcategoria::orderBy('id', 'desc')->get();
-        return view('createArtigo', compact('artigo', 'categoria', 'tipo','subcategoria'));
+        $armazem = Armazem::orderBy('id', 'desc')->get();
+        $unidade = Unidade::orderBy('id', 'desc')->get();
+        return view('createArtigo', compact('categoria', 'tipo','subcategoria','armazem','unidade'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    public function validation()
+    {
+        
+    }
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'codigobarra' => 'required|numeric',
+            'nome' => 'required|unique:artigo,nome|min:5',
+            'categoria_id' => 'required',
+            'subcategoria_id' => 'required',
+            'tipo_id' => 'required',
+            'preco' => 'required',
+            'iva' => 'required',
+            'desconto' => 'required',
+            'armazem_id' => 'required',
+            'quantidade' => 'required',
+            'stockminimo' => 'required',
+            'icon' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $input = $request->all();
+        $stocki = $request->all();
+        if($icon = $request->file('icon')){
+            $destino = 'assets/images/artigo';
+            $perfil = date('YmdHis') . "." . $icon->getClientOriginalExtension();
+            $icon->move($destino, $perfil);
+            $input['icon'] = "$perfil";
+        } else {
+            unset($input['icon']);
+        }
+
+        $artigo = Artigo::create($input);
+        $stocki['artigo_id'] = $artigo->id;
+        $stock = Stock::create($stocki);
+        if($stock);
+            {
+                $request->session()->flash('status', 'Artigo adicionada');
+                return redirect('artigo');
+            }
+            $request->session()->flash('status', 'Erro ao Adicionar!');
+            return redirect('artigo');
     }
 
     /**
@@ -65,19 +102,52 @@ class ArtigoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $artigo = Artigo::with(['categorias','subcategorias','tipos', 'stocks'])->orderBy('id', 'desc')->find($id);
+        $categoria = Categoria::orderBy('id', 'desc')->get();
+        $tipo = Tipo::orderBy('id', 'desc')->get();
+        $subcategoria = Subcategoria::orderBy('id', 'desc')->get();
+        $armazem = Armazem::orderBy('id', 'desc')->get();
+        $unidade = Unidade::orderBy('id', 'desc')->get();
+        return view('createArtigo', compact('artigo', 'categoria', 'tipo','subcategoria','armazem','unidade'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'codigobarra' => 'required|numeric',
+            'nome' => 'required',
+            'categoria_id' => 'required',
+            'subcategoria_id' => 'required',
+            'tipo_id' => 'required',
+            'preco' => 'required',
+            'iva' => 'required',
+            'desconto' => 'required',
+            'armazem_id' => 'required',
+            'quantidade' => 'required',
+            'stockminimo' => 'required',
+            'icon' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $input = $request->all();
+        $stocki = $request->all();
+        if($icon = $request->file('icon')){
+            $destino = 'assets/images/artigo';
+            $perfil = date('YmdHis') . "." . $icon->getClientOriginalExtension();
+            $icon->move($destino, $perfil);
+            $input['icon'] = "$perfil";
+        } else {
+            unset($input['icon']);
+        }
+
+        $stock = Artigo::find($id)->update($input);
+        /* $stock = Stock::where('artigo_id', $id)->get(); */
+        if($stock);
+            {
+                $request->session()->flash('status', 'Artigo Alterada');
+                return redirect('artigo');
+            }
+            $request->session()->flash('status', 'Erro ao Alterar!');
+            return redirect('artigo');
     }
 
     /**
